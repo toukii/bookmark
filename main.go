9868e767ee081fa6868e77f9e6ef25f4a77ce27c
shaalx <shaalx@yahoo.com.sg>
2015-12-20 21:07:06 +0800

    auto

--- main.go
+++ main.go
@@ -211,6 +211,6 @@ func webhook(rw http.ResponseWriter, req *http.Request) {
 		http.Redirect(rw, req, "/", 302)
 		return
 	}
-	command.Reset("git pull origin master").Execute()
+	command.Reset("git pull origin master:master").Execute()
 	updateMD(rw, req)
 }
